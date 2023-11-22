6f778c0d4725750b896ae255efd1b1902ee6ac4f
kpooja-digi <141310630+kpooja-digi@users.noreply.github.com>
2023-11-22 11:17:49 +0530

    implemented tailwid

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,11 @@
+// /**@type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+const config = require('tailwind-config/tailwind.config.js')
+
+module.exports = config
